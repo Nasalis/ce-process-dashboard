18067a0f4634fd3f86bfba3fd93d40a33af075ff
Davi dos Santos <davidossantosfreitas@hotmail.com>
2021-07-26 09:48:03 -0300

    Componentes TableArea e Button criados e inseridos na pagina principal do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { GlobalFooter } from './components/GlobalFooter';
 import { GlobalHeader } from './components/GlobalHeader';
+import { TableArea } from './components/TableArea';
 
 function App() {
   return (
     <div className="App">
       <GlobalHeader/>
+      <TableArea/>
       <GlobalFooter/>
     </div>
   );

ea69eb667cddd2641a9563b0985017d757b5a896
Davi dos Santos <davidossantosfreitas@hotmail.com>
2021-07-26 08:52:11 -0300

    GlobalHeader inserido

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { GlobalHeader } from './components/GlobalHeader';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <GlobalHeader/>
     </div>
   );
 }

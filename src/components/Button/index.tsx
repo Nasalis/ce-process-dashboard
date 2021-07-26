85bc689921b1b294a9f2f272c7fafe6d5afb591e
Davi dos Santos <davidossantosfreitas@hotmail.com>
2021-07-26 15:04:03 -0300

    type Props de GeneralButton atualizado

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,13 +8,15 @@ import style from './style.module.scss'
 type Props = ButtonProps & {
     text: string,
     width: string,
+    action?: () => void;
 }
 
-export function GeneralButton({text, width, ...rest}: Props) {
+export function GeneralButton({text, width, action, ...rest}: Props) {
     return (
         <Button 
             className={style.buttonContainer}
             style={{width}}
+            onClick={action}
             {...rest}
         >
             {text}

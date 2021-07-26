a420339322aa2fd8c479f7ffd3fc107e570ec840
Davi dos Santos <davidossantosfreitas@hotmail.com>
2021-07-26 09:21:11 -0300

    GlobalFooter criado e inserido no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import { GlobalFooter } from './components/GlobalFooter';
 import { GlobalHeader } from './components/GlobalHeader';
 
 function App() {
   return (
     <div className="App">
       <GlobalHeader/>
+      <GlobalFooter/>
     </div>
   );
 }

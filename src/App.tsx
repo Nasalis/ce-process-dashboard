10435a6043fcbe13f10139145ed7c85f883037b1
Davi dos Santos <davidossantosfreitas@hotmail.com>
2021-07-27 15:32:01 -0300

    definicao de rotas com react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
 import './App.css';
-import { GlobalFooter } from './components/GlobalFooter';
 import { GlobalHeader } from './components/GlobalHeader';
-import { TableArea } from './components/TableArea';
+import { GlobalFooter } from './components/GlobalFooter';
+import { Home } from './components/Home';
+import { ChartPage } from './components/ChartPage';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <GlobalHeader/>
-      <TableArea/>
-      <GlobalFooter/>
-    </div>
+    <Router>
+      <div className="App">
+        <GlobalHeader/>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/chart" exact component={ChartPage} />
+        </Switch>
+        <GlobalFooter/>
+      </div>
+    </Router>
   );
 }
 
